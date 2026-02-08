9d18b2e115b86ecfb00b4a10aa94f3c5137baf23
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2026-02-07 19:19:40 -0600

    third version

--- app/src/main/java/com/wolf/beephour/presentation/BootReceiver.kt
+++ app/src/main/java/com/wolf/beephour/presentation/BootReceiver.kt
@@ -8,7 +8,7 @@ class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             val prefs = context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
-            val alarmEnabled = prefs.getBoolean("hourly_alarm_enabled", true)
+            val alarmEnabled = prefs.getBoolean("hourly_alarm_enabled", false)
             if (alarmEnabled) {
                 HourlyBeepReceiver.scheduleNextHour(context)
             }
--- app/src/main/java/com/wolf/beephour/presentation/HourlyBeepReceiver.kt
+++ app/src/main/java/com/wolf/beephour/presentation/HourlyBeepReceiver.kt
@@ -2,15 +2,16 @@ package com.wolf.beephour.presentation
 
 import android.annotation.SuppressLint
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.VibrationEffect
-import android.os.Vibrator
+import android.os.VibratorManager
 import android.util.Log
 import com.wolf.beephour.R
 import java.util.Calendar
@@ -18,39 +19,9 @@ import java.util.Calendar
 class HourlyBeepReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val prefs = context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
-        val enabled = prefs.getBoolean("hourly_alarm_enabled", true)
+        if (!prefs.getBoolean("hourly_alarm_enabled", true)) return
 
-        Log.e("test", "check enabled")
-        if (!enabled) return   // No hacer nada
-
-        // Reproducir sonido
-        val afd = context.resources.openRawResourceFd(R.raw.beepbeep)
-        val mediaPlayer = MediaPlayer().apply {
-            setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_ALARM)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .build()
-            )
-            setDataSource(
-                afd.fileDescriptor,
-                afd.startOffset,
-                afd.length
-            )
-            prepare()
-            start()
-        }
-        afd.close()
-        mediaPlayer.setOnCompletionListener {
-            it.release()
-        }
-
-        val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        vibrator.vibrate(
-            VibrationEffect.createOneShot(300, VibrationEffect.DEFAULT_AMPLITUDE)
-        )
-
-        // Programar siguiente alarma
+        playHourlyFeedback(context)
         scheduleNextHour(context)
     }
 
@@ -59,7 +30,7 @@ class HourlyBeepReceiver : BroadcastReceiver() {
         fun scheduleNextHour(context: Context) {
             val calendar = Calendar.getInstance()
             calendar.add(Calendar.HOUR_OF_DAY, 1)
-            calendar.set(Calendar.MINUTE, 1)
+            calendar.set(Calendar.MINUTE, 0)
             calendar.set(Calendar.SECOND, 0)
             calendar.set(Calendar.MILLISECOND, 0)
 
@@ -78,5 +49,60 @@ class HourlyBeepReceiver : BroadcastReceiver() {
                 pendingIntent
             )
         }
+    }
+
+    private fun playHourlyFeedback(context: Context) {
+        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+        val ringerMode = audioManager.ringerMode
+        val isSilent = ringerMode == AudioManager.RINGER_MODE_SILENT
+        val isVibrate = ringerMode == AudioManager.RINGER_MODE_VIBRATE
+        val isDndEnabled = notificationManager.currentInterruptionFilter !=
+                NotificationManager.INTERRUPTION_FILTER_ALL
+        if (isSilent || isDndEnabled) {
+            Log.e("test", "1")
+            return
+        } else if (isVibrate) {
+            Log.e("test", "2")
+            vibrate(context)
+        } else {
+            Log.e("test", "3")
+            playBeep(context)
+            vibrate(context)
+        }
+    }
+
+    private fun playBeep(context: Context) {
+        val afd = context.resources.openRawResourceFd(R.raw.beepbeep)
+        val attributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(attributes)
+            setDataSource(
+                afd.fileDescriptor,
+                afd.startOffset,
+                afd.length
+            )
+            prepare()
+            start()
+        }
+        mediaPlayer.setOnCompletionListener {
+            it.release()
+            afd.close()
+        }
+    }
+
+    private fun vibrate(context: Context) {
+        val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+        val vibrator = vibratorManager.defaultVibrator
+        vibrator.vibrate(
+            VibrationEffect.createOneShot(
+                300, VibrationEffect.DEFAULT_AMPLITUDE
+            )
+        )
     }
 }
